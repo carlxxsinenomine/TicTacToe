17315fab90515e666cfe2e6b09c68978439f527f
est_calexx <10johannesmunoz@gmail.com>
2025-10-14 11:32:40 +0800

    ADDED: buttons and main title screen

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,5 @@
+import 'dart:math';
+
 import 'package:flutter/material.dart';
 
 void main() => runApp(TicTacToe());
@@ -5,7 +7,9 @@ void main() => runApp(TicTacToe());
 class TicTacToe extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
+
     return MaterialApp(
+      title: 'TicTacToe',
       theme: ThemeData.dark().copyWith(
         useMaterial3: false, // dunno but without this, my custom style won't take effect
         scaffoldBackgroundColor: Color(0xFF132440), //  Change the bg color of the Scaffold
@@ -15,13 +19,71 @@ class TicTacToe extends StatelessWidget {
 
       ),
       home: Scaffold(
-        appBar: AppBar(
-          title: Center(
-              child: Text("TicTacToe")
-          ),
+        body: Column(
+          crossAxisAlignment: CrossAxisAlignment.stretch, // Stretch relative horizontally since it's a Column
+          mainAxisAlignment: MainAxisAlignment.center, // Vertical alignment
+          children: <Widget>[
+            // Title
+            Text(
+              'Tic-Tac-Toe',
+              textAlign: TextAlign.center,
+              style: TextStyle(
+                fontFamily: "Courgette",
+                fontSize: 50,
+              ),
+            ),
+
+            // center the button cause when crossAxis is stretched
+            // the width of the button is also stretched
+            // so centering makes the MainScreenButton relative to the Center widget
+            Center(
+              child: MainScreenButton(
+                titleLabel: 'Play with AI',
+                width: 200,
+                onPressed: () {
+                },
+              ),
+            ),
+            Center(
+              child: MainScreenButton(
+                titleLabel: 'Play with a friend',
+                width: 200,
+                onPressed: () {
+                },
+              ),
+            ),
+          ],
         ),
       ),
     );
   }
 }
 
+class MainScreenButton extends StatelessWidget {
+  final String titleLabel;
+  final VoidCallback? onPressed;
+  final double width;
+
+  const MainScreenButton({
+    required this.titleLabel,
+    this.onPressed,
+    this.width = 150,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return RawMaterialButton(
+      onPressed: onPressed,
+      elevation: 0,
+      fillColor: Color(0xFF16476A),
+      constraints: BoxConstraints.tightFor(width: width, height: 56),
+      shape: RoundedRectangleBorder(
+        borderRadius: BorderRadius.circular(28),
+      ),
+      child: Text(
+        titleLabel,
+        style: TextStyle(color: Colors.white),
+      ),
+    );
+  }
+}
